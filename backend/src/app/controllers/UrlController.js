68b857419508f129e9e0ad22c738a6ab6cb4c6c9
Ibcs16 <iagobrayhamcunha@hotmail.com>
2019-12-11 02:24:53 -0300

    Add url shortening feature
    The systems needed to create shortened url
    given a longer url and store it in db

---
+++ backend/src/app/controllers/UrlController.js
@@ -0,0 +1,35 @@
+import validateUrl from 'valid-url';
+import shortid from 'shortid';
+import Url from '../models/Url';
+
+require('dotenv').config();
+
+export default {
+  async store(req, res) {
+    const { longUrl } = req.body;
+
+    // Check if it's an actual url
+    if (validateUrl.isUri(longUrl)) {
+      try {
+        // Generates new ID
+        const code = shortid.generate();
+        const url = await Url.create({
+          shortUrl: `${process.env.BASE_URL}/${code}`,
+          longUrl,
+        });
+
+        // If not found, send error
+        if (!url) {
+          return res.status(404).json({ error: 'Url not found' });
+        }
+
+        return res.json(url);
+      } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Server error' });
+      }
+    } else {
+      return res.status(401).json({ error: 'Invalid url' });
+    }
+  },
+};
