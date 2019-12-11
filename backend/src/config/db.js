8cbc733d57648f97cb547930467cb14e4dd11ee2
Ibcs16 <iagobrayhamcunha@hotmail.com>
2019-12-11 02:20:00 -0300

    Add configuration for mongo connection

---
+++ backend/src/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+require('dotenv').config();
+
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('CONNECTED TO MONGODB');
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+export default connectToDb;
