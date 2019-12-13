447fb6e6ef53132ed77bd3fc9d51e1afe8e6d0dd
Ibcs16 <iagobrayhamcunha@hotmail.com>
2019-12-13 14:41:04 -0300

    Add show method to analytics

---
+++ backend/src/app/controllers/AnalyticsController.js
@@ -0,0 +1,19 @@
+import validateUrl from 'valid-url';
+import Url from '../models/Url';
+import client from '../../config/redis';
+
+require('dotenv').config();
+
+export default {
+  async show(req, res) {
+    const { shortUrl } = req.params;
+    
+    const url = Url.findeOne({shortUrl});
+
+    if(!url) {
+        return res.status(404).json({ error: 'Url not found' });
+    }
+      
+    return res.json(url);
+  },
+};
