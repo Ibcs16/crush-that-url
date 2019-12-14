949838b58c0c1e03b166d462c62f96c4066faede
Ibcs16 <iagobrayhamcunha@hotmail.com>
2019-12-14 20:45:32 -0300

    Add retrieve analytics for url

--- backend/src/app/controllers/AnalyticsController.js
+++ backend/src/app/controllers/AnalyticsController.js
@@ -1,21 +1,15 @@
-import validateUrl from 'valid-url';
 import Url from '../models/Url';
-import client from '../../config/redis';
-
-require('dotenv').config({
-    path: process.env.NODE_ENV ==='test' ? '.env.test' : '.env'
-});
 
 export default {
   async show(req, res) {
-    const { shortUrl } = req.params;
+    const { code } = req.params;
     
-    const url = Url.findeOne({shortUrl});
+    const url = await Url.findOne({code});
 
     if(!url) {
         return res.status(404).json({ error: 'Url not found' });
     }
-      
+
     return res.json(url);
   },
 };
