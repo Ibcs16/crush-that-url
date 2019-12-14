e53a596ab43983a93870f0e7ecffa330de17131a
Ibcs16 <iagobrayhamcunha@hotmail.com>
2019-12-14 20:44:18 -0300

    Add analytics route and fix
    redirection route parameter

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import UrlController from './app/controllers/UrlController';
 import RedirectController from './app/controllers/RedirectController';
+import AnalyticsController from './app/controllers/AnalyticsController';
 
 const routes = express.Router();
 
 // Defining routes
-routes.get('/:code', RedirectController.show);
+routes.get('/redirect/:code', RedirectController.show);
+routes.get('/analytics/:code', AnalyticsController.show);
 routes.post('/create', UrlController.store);
 
 export default routes;
