e9f434ece04039207653a3d65d798c0be79422ff
Ibcs16 <iagobrayhamcunha@hotmail.com>
2019-12-16 15:30:47 -0300

    Test continous integradtion with new method

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,9 @@ import UrlController from './app/controllers/UrlController';
 import express from 'express';
 
 const routes = express.Router();
-
+routes.get('/', (req, res) => {
+  return res.json({ message: 'ok' });
+});
 // Defining routes
 routes.put('/redirect/:code', RedirectController.show);
 routes.get('/analytics/:code', AnalyticsController.show);
